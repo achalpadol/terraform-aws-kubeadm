aefadba3d9403d6a971ce611f8d577989f5e248e
Achal Sunil Padol <achalpadol@gmail.com>
2026-08-13 11:55:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -64,13 +64,13 @@ variable "pod_network_cidr_block" {
 variable "master_instance_type" {
   type        = string
   description = "EC2 instance type for the master node (must have at least 2 CPUs and 2 GB RAM)."
-  default     = "t3.large"
+  default     = "c7i-flex.large"
 }
 
 variable "worker_instance_type" {
   type        = string
   description = "EC2 instance type for the worker nodes."
-  default     = "t3.large"
+  default     = "c7i-flex.large"
 }
 
 variable "num_workers" {
